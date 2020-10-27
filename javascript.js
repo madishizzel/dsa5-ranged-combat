f0759554090ca1565d3220d95f0d214f43e16d48
madishizzel <59286957+madishizzel@users.noreply.github.com>
2020-10-27 16:29:56 +0100

    new

---
+++ javascript.js
@@ -0,0 +1,61 @@
+var Heroes = [{Name:"Yrgel", FK:20}, {Name:"Yzabilla", FK:16}]; //List of Heroes with Name, FK-Wert, Weapons
+var Marks = {Hero:0};
+
+//Check if run as part of index.html Creation or as main
+var IsMain = false;
+try {
+  document.getElementById("ScriptedHTML").innerHTML = "Testing"
+} catch (ReferenceError) {
+  IsMain = true
+};
+
+function main() {
+  Marks = LoadSavedData();
+  RefreshHTML()
+};
+
+function changeMark(Key, Value) {
+  Marks[`${Key}`] = Value;
+  UpdateSavedData();
+  RefreshHTML()
+};
+
+function UpdateSavedData() {
+  try {
+    window.sessionStorage.setItem("savedMarks", Marks)
+  } catch (ReferenceError) {}
+};
+
+function LoadSavedData() {
+  let Temp = Marks;
+  try {
+    return window.sessionStorage.getItem("savedMarks")
+  } catch (ReferenceError) {
+    return Temp
+  }
+};
+
+function RefreshHTML () {
+  let OutputHTML = "";
+  OutputHTML += "<ul>";
+  OutputHTML += "<li><b>Helden:</b><ul>";
+  for (Hero in Heroes) {
+    if (Hero == Marks.Hero){
+      OutputHTML += `<li oncklick="changeMark("Hero",${Hero})"><mark>${Heroes[Hero].Name}</mark>`
+    } else {
+      OutputHTML += `<li oncklick="changeMark("Hero",${Hero})">${Heroes[Hero].Name}`
+    };
+    if (Hero+1 == Heroes.length) {
+      OutputHTML += "</ul>"
+    }
+  };
+  OutputHTML += "</ul>";
+  if(IsMain) {
+    console.log(OutputHTML)
+  } else {
+    document.getElementById("ScriptedHTML").innerHTML = OutputHTML;
+  }
+};
+
+//Run and display result or pass to index.html
+main();
